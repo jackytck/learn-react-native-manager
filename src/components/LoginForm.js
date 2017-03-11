109ee4f11843cbb410badcf3026b2c20eb23c8ab
Jacky Tang <jackytck@gmail.com>
2017-03-11 14:23:23 +0800

    :construction: add LoginForm component

---
+++ src/components/LoginForm.js
@@ -0,0 +1,49 @@
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+import { emailChanged } from '../actions'
+import {
+  Card,
+  CardSection,
+  Input,
+  Button
+} from './common'
+
+class LoginForm extends Component {
+  onEmailChange (text) {
+    this.props.emailChanged(text)
+  }
+
+  render () {
+    return (
+      <Card>
+        <CardSection>
+          <Input
+            label='Email'
+            placeholder='email@gmail.com'
+            onChangeText={this.onEmailChange.bind(this)}
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            secureTextEntry
+            label='Password'
+            placeholder='password'
+          />
+        </CardSection>
+
+        <CardSection>
+          <Button>
+            Login
+          </Button>
+        </CardSection>
+      </Card>
+    )
+  }
+}
+
+LoginForm.propTypes = {
+  emailChanged: PropTypes.func
+}
+
+export default connect(null, { emailChanged })(LoginForm)
