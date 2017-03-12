c410912b56cceef18ae7e7f3e1306307ec5fb3a2
Jacky Tang <jackytck@gmail.com>
2017-03-12 12:46:54 +0800

    :construction: navigate to EmployeeList

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Scene, Router } from 'react-native-router-flux'
 import LoginForm from './components/LoginForm'
+import EmployeeList from './components/EmployeeList'
 
 const RouterComponent = () => {
   return (
     <Router sceneStyle={{ paddingTop: 65 }}>
-      <Scene key='login' component={LoginForm} title='Please Login' />
+      <Scene key='auth'>
+        <Scene key='login' component={LoginForm} title='Please Login' />
+      </Scene>
+      <Scene key='main'>
+        <Scene key='employeelist' component={EmployeeList} title='Employees' />
+      </Scene>
     </Router>
   )
 }
