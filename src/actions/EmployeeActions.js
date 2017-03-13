1d2f57123bd993d0e38e05a79ab02e8d1d0e60fa
Jacky Tang <jackytck@gmail.com>
2017-03-13 22:55:31 +0800

    :sparkles: save employee record in firebase

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,3 +1,4 @@
+import firebase from 'firebase'
 import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE } from './types'
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -8,7 +9,10 @@ export const employeeUpdate = ({ prop, value }) => {
 }
 
 export const employeeCreate = ({ name, phone, shift }) => {
-  console.log(name, phone, shift)
+  const { currentUser } = firebase.auth()
+
+  firebase.database().ref(`/users/${currentUser.uid}/employees`)
+    .push({ name, phone, shift })
 
   return {
     type: EMPLOYEE_CREATE,
