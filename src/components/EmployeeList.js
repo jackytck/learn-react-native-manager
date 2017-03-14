4838735aa1551f941290d43c9e4f4ac696cbbece
Jacky Tang <jackytck@gmail.com>
2017-03-15 00:27:19 +0800

    :construction: fetch employees from firebase

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,11 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
 import {
   View,
-  Text
+  Text,
+  ListView
 } from 'react-native'
+import { map } from 'lodash'
+import { employeeFetch } from '../actions'
 
 class EmployeeList extends Component {
+  componentWillMount () {
+    this.props.employeeFetch()
+    this.createDataSource(this.props)
+  }
+
+  componentWillReceiveProps (nextProps) {
+    // nextProps are the next set of props that this component will be rendered with
+    // this.props is still the old set of props
+    this.createDataSource(nextProps)
+  }
+
+  createDataSource ({ employees }) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    })
+
+    this.dataSource = ds.cloneWithRows(employees)
+  }
+
   render () {
+    console.log(this.props)
     return (
       <View>
         <Text>Employee List</Text>
@@ -19,4 +43,15 @@ class EmployeeList extends Component {
   }
 }
 
-export default EmployeeList
+EmployeeList.propTypes = {
+  employeeFetch: PropTypes.func
+}
+
+const mapStateToProps = state => {
+  const employees = map(state.employees, (val, uid) => {
+    return { ...val, uid }
+  })
+  return { employees }
+}
+
+export default connect(mapStateToProps, { employeeFetch })(EmployeeList)
