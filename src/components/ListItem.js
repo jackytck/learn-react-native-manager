d92256cd02ea19215755d77c9cb4a6e0fb2caf8a
Jacky Tang <jackytck@gmail.com>
2017-03-15 00:35:15 +0800

    :construction: display employees in ListView

---
+++ src/components/ListItem.js
@@ -0,0 +1,30 @@
+import React, { Component, PropTypes } from 'react'
+import { Text } from 'react-native'
+import { CardSection } from './common'
+
+class ListItem extends Component {
+  render () {
+    const { name } = this.props.employee
+
+    return (
+      <CardSection>
+        <Text style={styles.titleStyle}>
+          {name}
+        </Text>
+      </CardSection>
+    )
+  }
+}
+
+ListItem.propTypes = {
+  employee: PropTypes.object
+}
+
+const styles = {
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 15
+  }
+}
+
+export default ListItem
